d686ae3c9829aef6db02990be982a7a129893fbd
Bruno Paes <brunopaes05@gmail.com>
2018-11-03 01:58:53 -0300

    driver path correction

--- Scripts/Scraper/scraper.py
+++ Scripts/Scraper/scraper.py
@@ -6,7 +6,7 @@ import time
 class Scraper(object):
 
     def __init__(self):
-        self.path = r'/Users/brunopaes/Documents/OneDrive/Acadêmico/ESPM/Misc/05.4 - Playground/Drivers/chromedriver'
+        self.path = r'/Users/brunopaes/Documents/OneDrive/Acadêmico/ESPM/Misc/05.4 - Python_Playground/Drivers/chromedriver'
         self.driver = webdriver.Chrome(self.path)
         self.url = 'https://web.whatsapp.com'
 
@@ -14,10 +14,10 @@ class Scraper(object):
         self.driver.get(self.url)
         time.sleep(15)
 
-        elem = self.driver.find_element_by_xpath('//span[contains(text(),"Bruna Franco")]')
+        elem = self.driver.find_element_by_xpath('//span[contains(text(),"Guilherme Heitzmann")]')
         elem.click()
 
-        for i in range(100):
+        for i in range(10):
             input_box = self.driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')
             input_box.send_keys('Treme')
             input_box.send_keys(Keys.ENTER)
