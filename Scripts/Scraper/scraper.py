e1ad828f8164fbe484f3a42ba538906f7d0d1d12
Bruno Paes <brunopaes05@gmail.com>
2018-11-03 21:45:48 -0300

    Multiple Messages
    Iterating through message lists

--- Scripts/Scraper/scraper.py
+++ Scripts/Scraper/scraper.py
@@ -17,14 +17,17 @@ class Scraper(object):
         self.driver.get(self.url)
         time.sleep(self.time)
 
-        elem = self.driver.find_element_by_xpath('//span[contains(text(),"Pedro Gucciardi")]')
+        elem = self.driver.find_element_by_xpath('//span[contains(text(),"Baguete dos Meteoros")]')
         elem.click()
 
+        msg = ['Opa', 'que', 'legal!', '\o/']
+
         for i in range(2):
-            input_box = self.driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')
-            input_box.send_keys('Teste {}'.format(i))
-            input_box.send_keys(Keys.ENTER)
-            time.sleep(1)
+            for j in range(len(msg)):
+                input_box = self.driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')
+                input_box.send_keys(msg[j])
+                input_box.send_keys(Keys.ENTER)
+                time.sleep(1)
 
     def main(self):
         self.search()
