9d526d2d3e77321da8c6cbf13893c1eb83a0c77b
Bruno Paes <brunopaes05@gmail.com>
2019-02-20 17:38:21 -0300

    Commit

--- Scripts/Scraper/scraper.py
+++ Scripts/Scraper/scraper.py
@@ -17,17 +17,16 @@ class Scraper(object):
         self.driver.get(self.url)
         time.sleep(self.time)
 
-        elem = self.driver.find_element_by_xpath('//span[contains(text(),"Renato Paes")]')
+        elem = self.driver.find_element_by_xpath('//span[contains(text(),"Pedro Mochila")]')
         elem.click()
 
-        msg = 'A chuva parou?'
+        msg = 'D E U S   G R E G O  S2'
 
         while True:
             # for j in range(len(msg)):
             input_box = self.driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')
             input_box.send_keys(msg)
             input_box.send_keys(Keys.ENTER)
-            time.sleep(1)
 
     def main(self):
         self.search()
