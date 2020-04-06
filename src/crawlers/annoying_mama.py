9b401b0d28f289cc2dbbc372e37f0c1917cc3ad0
bruno <brunopaes05@gmail.com>
2020-04-06 10:55:56 -0300

    Adding Chromedriver.exe and refactoring wpp crawler

--- src/crawlers/annoying_mama.py
+++ src/crawlers/annoying_mama.py
@@ -1,13 +1,15 @@
+from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
+
 import time
 
 
-class Scraper(object):
+class AnnoyingMama:
     def __init__(self):
-        self.path = r'/Users/brunopaes/Documents/OneDrive/Acadêmico/ESPM/misc/05.4 - Python_Playground/drivers/chromedriver'
+        self.path = r'C:\Users\bruno\PycharmProjects\python-sandbox\drivers\chromedriver.exe'
         self.options = webdriver.ChromeOptions()
         self.options.add_argument('--user-data-dir=./User_Data')
-        self.driver = webdriver.Chrome(self.path, chrome_options=self.options)
+        self.driver = webdriver.Chrome(self.path, options=self.options)
         self.url = 'https://web.whatsapp.com'
 
     def access_page(self):
@@ -15,22 +17,20 @@ class Scraper(object):
 
         input()
 
-        elem = self.driver.find_element_by_xpath('//span[contains(text(),"Boiçucanga The Mission")]')
-        elem.click()
+        chat = self.driver.find_element_by_xpath('//span[contains(text(),"Cristiane Augusto")]')
+        chat.click()
 
-        # message = self.driver.find_elements_by_css_selector('#main > div._3zJZ2 > div > div > div._9tCEa > div:nth-child(11) > div')
-        message = self.driver.find_element_by_class_name('_3_7SH Zq3Mc tail')
+        for i in range(10000):
+            input_box = self.driver.find_element_by_xpath(
+                '//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')
+            input_box.send_keys('PARA DE ME ENCHER!!')
+            input_box.send_keys(Keys.ENTER)
 
-        print(len(message))
-
-        for i in message:
-            print(i.text)
+            time.sleep(2)
 
     def __call__(self, *args, **kwargs):
         self.access_page()
 
-        input()
-
 
 if __name__ == '__main__':
-    Scraper()()
+    AnnoyingMama().__call__()
