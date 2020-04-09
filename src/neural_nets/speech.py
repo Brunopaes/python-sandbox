38f91274302f7b0e1a8482f464a03bc5fe4116eb
bruno <brunopaes05@gmail.com>
2020-04-09 15:18:48 -0300

    Adding Chromedriver.exe and refactoring wpp crawler

--- src/neural_nets/speech.py
+++ src/neural_nets/speech.py
@@ -6,37 +6,48 @@ import io
 import os
 
 
-def sample_recognize(local_file_path):
-    """
-    Transcribe a short audio file using synchronous speech recognition
+def set_path():
+    """This function sets settings.json in PATH.
 
-    Args:
-      local_file_path Path to local audio file, e.g. /path/audio.wav
-    """
+    Returns
+    -------
 
-    path = r"C:\Users\bruno\PycharmProjects\python-sandbox\src\neural_nets" \
-           r"\settings.json"
+    """
+    path = os.path.abspath('src/neural_nets/settings.json')
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = path
+
+
+def sample_recognize(local_file_path):
+    """This function transcribe a short audio file using synchronous speech
+    recognition.
+
+    Parameters
+    ----------
+    local_file_path : Path to local audio file, e.g. /path/audio.wav
+
+    Returns
+    -------
+
+    """
+    set_path()
     client = speech_v1.SpeechClient()
 
-    language_code = "pt-BR"
-    sample_rate_hertz = 16000
-    encoding = enums.RecognitionConfig.AudioEncoding.ENCODING_UNSPECIFIED
-
-    config = {
-        "language_code": language_code,
-        "sample_rate_hertz": sample_rate_hertz,
-        "encoding": encoding,
-    }
     with io.open(local_file_path, "rb") as f:
         content = f.read()
-    audio = {"content": content}
 
-    response = client.recognize(config, audio)
+    audio = {
+        "content": content
+    }
+
+    response = client.recognize({
+        "language_code": "pt-BR",
+        "sample_rate_hertz": 16000,
+        "encoding": enums.RecognitionConfig.AudioEncoding.ENCODING_UNSPECIFIED,
+    }, audio)
+
     for result in response.results:
-        # First alternative is the most probable result
         alternative = result.alternatives[0]
         print(u"{}".format(alternative.transcript))
 
 
-sample_recognize(r'C:\Users\bruno\Downloads\4.m4a')
+sample_recognize(r'C:\Users\bruno\PycharmProjects\python-sandbox\data\1.mp3')
