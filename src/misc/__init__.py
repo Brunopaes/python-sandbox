8a7cd4efd120e2df2a04e35518cba63cf37f2b0d
Bruno <brunopaes05@gmail.com>
2020-07-02 11:47:31 -0300

    Docsstringss

--- src/misc/__init__.py
+++ src/misc/__init__.py
@@ -1,8 +1,12 @@
 __all__ = [
+    'dataset_class_fixer',
     'elo_classifier',
     'fausthanos',
+    'file_operations',
+    'helpers',
     'mail',
     'pdf_converter',
+    'reddit_analysis'
 ]
 
 from . import __all__
