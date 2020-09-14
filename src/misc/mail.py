5db3fe35c8c6093c96ccf09c0a96a9ade9b7ec14
bruno <brunopaes05@gmail.com>
2020-09-13 21:15:22 -0300

    Adding mail.py

--- src/misc/mail.py
+++ src/misc/mail.py
@@ -1,37 +1,113 @@
+# -*- coding: utf-8 -*-
 from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
 from email.mime.text import MIMEText
+from helpers import read_json
+from email import encoders
+
+import datetime
 import smtplib
 
 
-class SendMail(object):
-    def __init__(self):
-        self.email = 'ohhalho@gmail.com'
-        self.password = '13112766'
-        self.server = 'smtp.gmail.com'
-        self.port = 587
+def instantiating_mail(smtp_server, port, sender, token):
+    """This function instantiates the email session.
 
-        self.recipients = 'mrpm1@globo.com'
+    Parameters
+    ----------
+    smtp_server : str
+        smtp_server DNS address.
+    port : int
+        smtp_server port.
+    sender : str
+        The sender mail address.
+    token : str
+        Account´s token.
 
-        session = smtplib.SMTP(self.server, self.port)
-        session.ehlo()
-        session.starttls()
-        session.login(self.email, self.password)
-        self.session = session
+    Returns
+    -------
 
-    def send_message(self):
-        message = MIMEMultipart("alternative")
-        message["Subject"] = "Avengers: End Game"
-        message["From"] = self.email
-        message["To"] = self.recipients
+    """
+    session = smtplib.SMTP(smtp_server, port)
+    session.ehlo()
+    session.starttls()
+    session.login(sender, token)
 
-        message.attach(MIMEText('Viadoooooo'))
 
-        with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
-            server.login(self.email, self.password)
-            server.sendmail(self.email, message['To'].split(','), message.as_string())
+def mail(sender, to, subject, content=None, attachment=None, port=25,
+         token='', smtp_server='smtp.gmail.com'):
+    """This function send emails.
+
+    Parameters
+    ----------
+    sender : str
+        The sender mail address.
+    to : str
+        Recipient list.
+    subject : str
+        Message subject.
+    content : str, optional
+        Content subject.
+    attachment : str, optional
+        Attachment path.
+    port : int
+        smtp_server port.
+    token : str, optional
+        Account´s token.
+    smtp_server : str, optional
+        smtp_server DNS address.
+
+    Returns
+    -------
+
+    """
+    instantiating_mail(smtp_server, port, sender, token)
+
+    message = MIMEMultipart("alternative")
+    message["Subject"] = subject
+    message["From"] = sender
+    message["To"] = to
+
+    message.attach(MIMEText(content))
+    if attachment:
+        part = MIMEBase('application', "octet-stream")
+        if isinstance(attachment, str):
+            with open(attachment, 'rb') as f:
+                part.set_payload(f.read())
+
+            encoders.encode_base64(part)
+            part.add_header('Content-Disposition',
+                            'attachment; filename="{}"'.format(
+                                attachment.split('\\')[-1]
+                            ))
+        message.attach(part)
+
+    with smtplib.SMTP_SSL(smtp_server, 465) as server:
+        server.login(sender, token)
+        server.sendmail(sender, message['To'].split(','), message.as_string())
 
 
 if __name__ == '__main__':
-    while True:
-        SendMail().send_message()
-        print('email seent')
+    mail_settings = read_json('mail_settings.json')
+    mail(
+        mail_settings.get('sender'),
+        mail_settings.get('recipient'),
+        mail_settings.get('subject'),
+        """
+Olá,
+
+gostaria de reservar a academia para os seguintes dias/horários:
+    - Terça-Feira ({}): 19:00-20:00 (Bruno)
+    - Quinta-Feira ({}): 20:00-21:00 (Bruno & Verônica)
+
+Att,
+
+Bruno Paes & Verônica Brandt
+Imperial 53
++55 (11) 94258-4307
+
+        """.format(
+            datetime.date.today().replace(day=14) + datetime.timedelta(days=1),
+            datetime.date.today().replace(day=14) + datetime.timedelta(days=3),
+        ),
+        token=mail_settings.get('token'),
+    )
