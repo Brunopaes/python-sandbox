dbbfbcb07bbd280398664a4c7627b64ca2797b4f
bruno <brunopaes05@gmail.com>
2020-09-29 14:39:48 -0300

    Adding alert handler

---
+++ src/crawlers/briotto.py
@@ -0,0 +1,19 @@
+def saude(pessoas, d_encontro, d_observacao):
+    infected = []
+    if any(i > -1 for i in pessoas):
+        for j in range(pessoas.count(-1)):
+            pessoas[pessoas.index(-1)] = d_encontro
+
+    for i in pessoas:
+        if i + 15 > d_observacao:
+            infected.append(i)
+
+    return len(infected)
+
+
+if __name__ == '__main__':
+    print(saude(
+        [-1, 0, 3, -1, -1],
+        3,
+        17
+    ))
