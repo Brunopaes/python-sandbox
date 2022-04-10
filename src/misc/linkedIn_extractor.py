d017d82998841787b2ba1bd9dc77091de0cafed2
Brunopaes <brunopaes05@gmail.com>
2022-04-10 19:45:24 -0300

    elo

---
+++ src/misc/linkedIn_extractor.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+
+import pandas
+
+
+# driver = webdriver.Chrome(
+#     r"E:\PythonProjects\Personal\friday\data\webdriver\chromedriver.exe"
+# )
+
+df = pandas.read_excel('data/Database thesis v2.xlsx')
