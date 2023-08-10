3bf10d8239e2984cff38b5a8e21f27e6f16368a4
Brunopaes <brunopaes05@gmail.com>
2023-08-09 23:28:57 -0300

    Docstrings and Black

--- src/misc/aggregator.py
+++ src/misc/aggregator.py
@@ -1,93 +1,30 @@
-import datetime
-import random
 from collections import defaultdict
-from operator import itemgetter
 
-import pandas
+def aggregate_nested_dicts(payload_map, keys, values):
+    aggregated_map = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
 
+    for item in payload_map:
+        key_values = tuple((k, item.get(k, None)) for k in keys)
+        data_values = tuple((k, item.get(k, None)) for k in values)
 
-def generate_payload(records=1000):
-    """Generates mocked payloads to test aggregator function.
+        for v in values:
+            aggregated_map[key_values][v] += item.get(v, 0)
 
-    Parameters
-    ----------
-    records : int, optional
-        Records to generate.
+    aggregated_list = []
+    for key_values, value_map in aggregated_map.items():
+        data_dict = dict(key_values)
+        data_dict.update(value_map)
+        aggregated_list.append(data_dict)
 
-    Returns
-    -------
-    payload : list
-        Generated payload's list.
+    return aggregated_list
 
-    """
-    payload = []
-    for i in range(records):
-        item = {
-            "technology": random.choice(technologies),
-            "date": datetime.date.today().replace(day=random.randint(1, 30)),
-            "variable": random.choice(variables),
-            "value": random.randint(1, 100),
-        }
-        payload.append(item)
+# Example input
+input_payload = [
+    {"a": 1, "b": 2, "fd": 1, "gd": 5},
+    {"a": 1, "b": 2, "fd": 1, "gd": 10}
+]
+groupby_keys = ["a", "b"]
+values_to_aggregate = ["fd", "gd"]
 
-    return payload
-
-
-def aggregate_payload(payload, keys, values):
-    """Given a list of keys and values, aggregates a list of
-    dictionaries.
-
-    Parameters
-    ----------
-    payload : list
-        Data payload.
-
-    keys : list
-        Keys to aggregate.
-
-    values : str
-        Values to be aggregated.
-
-    Returns
-    -------
-    result : list
-        Aggregated data dictionaries.
-
-    Examples
-    --------
-    >>> payload_ = [
-    >>>     {"technology": "hydro", "date": datetime.date.today(), "variable": "hour_01", "value": 20}
-    >>>     {"technology": "hydro", "date": datetime.date.today(), "variable": "hour_01", "value": 10}
-    >>>     {"technology": "hydro", "date": datetime.date.today(), "variable": "hour_02", "value": 20}
-    >>> ]
-
-    >>> aggregate_payload(payload_, keys=["technology", "date", "variable"], value="value")
-
-    >>> [
-    >>>     {"technology": "hydro", "date": datetime.date.today(), "variable": "hour_01", "value": 30},
-    >>>     {"technology": "hydro", "date": datetime.date.today(), "variable": "hour_02", "value": 20}
-    >>> ]
-
-    """
-    key_fn = itemgetter(*keys)
-    groups = defaultdict(int)
-    for item in payload:
-        key = key_fn(item)
-        groups[key] += item[values]
-
-    return [dict(zip(keys + [values], key + (value,))) for key, value in groups.items()]
-
-
-if __name__ == "__main__":
-    technologies = ["hydro", "solar", "wind", "thermal", "geothermal"]
-    variables = [f"hour_{i:02d}" for i in range(1, 26)]
-
-    result = aggregate_payload(
-        generate_payload(10000), ["technology", "date", "variable"], "value"
-    )
-
-    print(
-        pandas.DataFrame.from_records(result).sort_values(
-            by=["technology", "date", "variable"]
-        )
-    )
+aggregated_result = aggregate_nested_dicts(input_payload, groupby_keys, values_to_aggregate)
+print(aggregated_result)
