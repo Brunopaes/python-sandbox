f99c3ec0c3b4a543e7ce8d6ac7b668d0a4f90075
Brunopaes <brunopaes05@gmail.com>
2023-08-31 21:53:20 -0300

    jmespath restructuring

---
+++ src/james.py
@@ -0,0 +1,55 @@
+import jmespath
+
+
+def restructure_dictionary(input_dict, query):
+    """
+    Restructures a non-nested dictionary using JMESPath query.
+
+    Args:
+        input_dict (dict): The dictionary to be restructured.
+        query (str): The JMESPath query specifying the new structure.
+
+    Returns:
+        dict: The restructured dictionary.
+    """
+    result = jmespath.search(query, input_dict)
+    return result
+
+
+# Example dictionary
+data = {
+    "seriesId": "SOLAR.GENERATION.01.SUM",  # BAIT
+    "seriesType": "GENERATION",  # BAIT
+    "aggregationHour": "01",  # BAIT
+    "bbdsId": "generate-from-key",
+    "ticker": "CLCGSOLA HR1 Index",
+    "frequency": "Daily",
+    "historyType": "Close",
+    "periodicity": "DAY",
+    "periodicityAggregationMethod": "SUM",
+    "transformation": "NOMINAL",
+    "technology": "SOLAR",  # BAIT
+    "powerPlantId": "NA",
+    "powerPlantName": "NA",
+    "generatorName": "NA",
+    "companyOwner": "NA",
+    "regionCode": "CL",
+    "regionName": "Chile",
+    "ardUnit": "MWh",
+    "unit": "MWh",
+    "scale": 1,
+    "precision": 0,
+    "seasonality": "NSA",
+    "referencePeriodEndDate": "2023-08-02",  # BAIT
+    "reportTime": "2023-08-01T03:00:00+00:00",  # BAIT
+    "reportTimeType": "BBG_DATA_PROCESSED_DATETIME",
+    "ardValue": 10.4,  # BAIT
+    "value": 10.4  # BAIT
+}
+
+# JMESPath query to restructure the dictionary
+query = "{bbdsId: bbdsId, tickerDetails: {ticker: ticker, historyType: historyType, frequency: frequency}, data: {scale: scale}}"
+
+# Restructure the dictionary using the function
+restructured_data = restructure_dictionary(data, query)
+print(restructured_data)
--- src/misc/testaa.py
+++ src/misc/testaa.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 def process_keys(payload, key_list):
     """
     Process the given keys in the payload dictionary.
@@ -43,3 +45,137 @@ def restructure_dict(payload, keys):
         dict: A restructured dictionary based on the provided keys.
     """
     return {outer_key: process_nested(payload, nested_keys) for outer_key, nested_keys in keys.items()}
+
+
+def aggregate_dicts(input_list):
+    aggregated_dict = defaultdict(list)
+
+    for item in input_list:
+        catalog_key = tuple(item["catalogKey"].items())
+        aggregated_dict[catalog_key].append(item["data"])
+
+    output_list = [{"catalogKey": dict(key), "data": value} for key, value in aggregated_dict.items()]
+    return output_list
+
+
+if __name__ == '__main__':
+    input_ = [
+        {
+            "seriesId": "SOLAR.GENERATION.01.SUM",  # BAIT
+            "seriesType": "GENERATION",  # BAIT
+            "aggregationHour": "01",  # BAIT
+            "bbdsId": "generate-from-key",
+            "ticker": "CLCGSOLA HR1 Index",
+            "frequency": "Daily",
+            "historyType": "Close",
+            "periodicity": "DAY",
+            "periodicityAggregationMethod": "SUM",
+            "transformation": "NOMINAL",
+            "technology": "SOLAR",  # BAIT
+            "powerPlantId": "NA",
+            "powerPlantName": "NA",
+            "generatorName": "NA",
+            "companyOwner": "NA",
+            "regionCode": "CL",
+            "regionName": "Chile",
+            "ardUnit": "MWh",
+            "unit": "MWh",
+            "scale": 1,
+            "precision": 0,
+            "seasonality": "NSA",
+            "referencePeriodEndDate": "2023-08-01",  # BAIT
+            "reportTime": "2023-08-01T03:00:00+00:00",  # BAIT
+            "reportTimeType": "BBG_DATA_PROCESSED_DATETIME",
+            "ardValue": 10.4,  # BAIT
+            "value": 10.4  # BAIT
+        },
+        {
+            "seriesId": "SOLAR.GENERATION.01.SUM",  # BAIT
+            "seriesType": "GENERATION",  # BAIT
+            "aggregationHour": "01",  # BAIT
+            "bbdsId": "generate-from-key",
+            "ticker": "CLCGSOLA HR1 Index",
+            "frequency": "Daily",
+            "historyType": "Close",
+            "periodicity": "DAY",
+            "periodicityAggregationMethod": "SUM",
+            "transformation": "NOMINAL",
+            "technology": "SOLAR",  # BAIT
+            "powerPlantId": "NA",
+            "powerPlantName": "NA",
+            "generatorName": "NA",
+            "companyOwner": "NA",
+            "regionCode": "CL",
+            "regionName": "Chile",
+            "ardUnit": "MWh",
+            "unit": "MWh",
+            "scale": 1,
+            "precision": 0,
+            "seasonality": "NSA",
+            "referencePeriodEndDate": "2023-08-02",  # BAIT
+            "reportTime": "2023-08-01T03:00:00+00:00",  # BAIT
+            "reportTimeType": "BBG_DATA_PROCESSED_DATETIME",
+            "ardValue": 10.4,  # BAIT
+            "value": 10.4  # BAIT
+        },
+        {
+            "seriesId": "SOLAR.GENERATION.02.SUM",  # BAIT
+            "seriesType": "GENERATION",  # BAIT
+            "aggregationHour": "02",  # BAIT
+            "bbdsId": "generate-from-key",
+            "ticker": "CLCGSOLA HR1 Index",
+            "frequency": "Daily",
+            "historyType": "Close",
+            "periodicity": "DAY",
+            "periodicityAggregationMethod": "SUM",
+            "transformation": "NOMINAL",
+            "technology": "SOLAR",  # BAIT
+            "powerPlantId": "NA",
+            "powerPlantName": "NA",
+            "generatorName": "NA",
+            "companyOwner": "NA",
+            "regionCode": "CL",
+            "regionName": "Chile",
+            "ardUnit": "MWh",
+            "unit": "MWh",
+            "scale": 1,
+            "precision": 0,
+            "seasonality": "NSA",
+            "referencePeriodEndDate": "2023-08-02",  # BAIT
+            "reportTime": "2023-08-01T03:00:00+00:00",  # BAIT
+            "reportTimeType": "BBG_DATA_PROCESSED_DATETIME",
+            "ardValue": 10.4,  # BAIT
+            "value": 10.4  # BAIT
+        },
+    ]
+
+    data_format = {
+        "catalogKey": ["aggregationHour", "seriesId", "seriesType"],
+        "data": {
+            "Key": ["bbdsId", "referencePeriodEndDate"],
+            "data": {
+                "ardUnit": "ardUnit",
+                "ardValue": "ardValue",
+                "periodicity": "periodicity",
+                "precision": "precision",
+                "referencePeriodEndDate": "referencePeriodEndDate",
+                "reportTimeType": "reportTimeType",
+                "scale": "scale",
+                "unit": "unit",
+                "value": "value"
+            },
+            "metadata": [],
+            "version": "1.0"
+        }
+    }
+
+    response = [restructure_dict(
+        i,
+        data_format
+    ) for i in input_]
+
+    # print(response)
+
+    res = aggregate_dicts(response)
+
+    print(res)
